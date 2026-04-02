eaa9b7d66616167968649c2a5a836d2960ee9e27
ТутоваЖелтова <katetutova008@mail.ru>
2026-04-02 16:26:06 +0300

    ybxt

--- fixme.cpp
+++ fixme.cpp
@@ -128,4 +128,3 @@ bsoncxx::builder::basic::document SessionAgregator::getFilter(std::string userLo
     filter.append(kvp(FieldCnst::LOGIN, userLogin.c_str()));
     return filter;
 }// Коммент 1
-
